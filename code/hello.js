a3c9a4bbe4226ac07ce1a75b7e6c2e63f3db4197
grguang <945455382@qq.com>
2019-09-29 14:04:42 +0800

    Add:     * design;

--- code/hello.js
+++ code/hello.js
@@ -4,6 +4,36 @@
  * @flow
  */
 
-module.exports.add = function add(a, b) {
-    return a + b;
-};
+function handleOffsetValueByPoint(layers) {
+    if (!layers || !layers.length) {
+        return { offsetPoint: [0, 0], width: 0, height: 0 };
+    }
+    const initX = layers[0].point[0];
+    const initY = layers[0].point[1];
+    const initWidth = layers[0].width;
+    const initHeight = layers[0].height;
+
+    let offsetX = initX;
+    let offsetY = initY;
+    let width = initX + initWidth;
+    let height = initY + initHeight;
+
+    for (let i = 1; i < layers.length; i++) {
+        const item = layers[i];
+        const { point, width: itemWidth, height: itemHeight } = item;
+        const [xValue, yValue] = point;
+        offsetX = Math.min(offsetX, xValue);
+        offsetY = Math.min(offsetY, yValue);
+        const xSub = xValue + itemWidth;
+        const ySub = yValue + itemHeight;
+        if (xSub > width) {
+            width = xSub;
+        }
+        if (ySub > height) {
+            height = ySub;
+        }
+    }
+    return { offsetPoint: [offsetX, offsetY], width: width - offsetX, height: height - offsetY };
+}
+
+exports.handleOffsetValueByPoint = handleOffsetValueByPoint;
--- test/hello.test.js
+++ test/hello.test.js
@@ -3,12 +3,158 @@
  *
  * @flow
  */
-const { add } = require('../code/hello');
+const { handleOffsetValueByPoint } = require('../code/hello');
 
-describe('add', () => {
-    it('add', () => {
-        expect(add(2,1)).toBe(3);
-        expect(add(2,2)).toBe(4);
-        expect(add(2,3)).toBe(5);
+describe('handleOffsetValueByPoint', () => {
+    it('component all in mainPad', () => {
+        const layers = [{
+           width: 50,
+           height: 30,
+           point: [100,50]
+        },{
+            width: 200,
+            height: 30,
+            point: [100,150]
+        },{
+            width: 80,
+            height: 30,
+            point: [125,200]
+        }];
+        const result = {
+            offsetPoint: [100,50],
+            width: 200,
+            height: 180,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component all in mainPad 2', () => {
+        const layers = [{
+           width: 75,
+           height: 30,
+           point: [200,50]
+        },{
+            width: 200,
+            height: 30,
+            point: [50,150]
+        },{
+            width: 50,
+            height: 30,
+            point: [50,250]
+        }];
+        const result = {
+            offsetPoint: [50,50],
+            width: 225,
+            height: 230,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component one in forkPad', () => {
+        const layers = [{
+           width: 75,
+           height: 30,
+           point: [-100,50]
+        },{
+            width: 200,
+            height: 30,
+            point: [50,100]
+        },{
+            width: 50,
+            height: 30,
+            point: [75,200]
+        }];
+        const result = {
+            offsetPoint: [-100,50],
+            width: 350,
+            height: 180,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component one in forkPad 1', () => {
+        const layers = [{
+           width: 250,
+           height: 30,
+           point: [-300,50]
+        },{
+            width: 200,
+            height: 30,
+            point: [50,100]
+        },{
+            width: 50,
+            height: 30,
+            point: [75,200]
+        }];
+        const result = {
+            offsetPoint: [-300,50],
+            width: 550,
+            height: 180,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component one in forkPad 2', () => {
+        const layers = [{
+           width: 50,
+           height: 30,
+           point: [-300,50]
+        },{
+            width: 200,
+            height: 30,
+            point: [50,100]
+        },{
+            width: 50,
+            height: 30,
+            point: [75,200]
+        }];
+        const result = {
+            offsetPoint: [-300,50],
+            width: 550,
+            height: 180,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component some in forkPad', () => {
+        const layers = [{
+           width: 300,
+           height: 30,
+           point: [50,-100]
+        },{
+            width: 150,
+            height: 30,
+            point: [50,75]
+        },{
+            width: 100,
+            height: 30,
+            point: [-150,50]
+        }];
+        const result = {
+            offsetPoint: [-150,-100],
+            width: 500,
+            height: 205,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
+    });
+    it('component some in forkPad 1', () => {
+        const layers = [{
+           width: 300,
+           height: 30,
+           point: [50,-100]
+        },{
+            width: 150,
+            height: 30,
+            point: [50,75]
+        },{
+            width: 100,
+            height: 30,
+            point: [-150,50]
+        },{
+            width: 250,
+            height: 30,
+            point: [150,150]
+        }];
+        const result = {
+            offsetPoint: [-150,-100],
+            width: 550,
+            height: 280,
+        };
+        expect(handleOffsetValueByPoint(layers)).toEqual(result);
     });
 });
